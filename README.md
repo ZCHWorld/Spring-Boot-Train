8d7e1abd5c556128204135b08406883523500f24
Zhao <284578220@qq.com>
2019-05-12 01:58:33 +0800

    创建新项目

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Spring-Boot-Train
-spring boot训练
+#### `描述：`spring boot功能训练
+
+**module**:
+
+1.**`thread：`**spring自带的多线程训练
---
+++ thread/src/main/java/com/example/thread/ThreadApplication.java
@@ -0,0 +1,25 @@
+package com.example.thread;
+
+import com.example.thread.config.TaskExecutorConfig;
+import com.example.thread.service.AsyncTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class ThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadApplication.class, args);
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+        AsyncTaskService asyncTaskService = context.getBean(AsyncTaskService.class);
+        for (int i = 0; i < 10; i++) {
+            asyncTaskService.executeAsyncTask(i);
+            asyncTaskService.executeAsyncTaskPlus(i);
+        }
+        context.close();
+    }
+
+}
